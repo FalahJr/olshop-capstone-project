b4e48b360921bcf1739bc7eed416956ce55ca90e
fatik07 <fatichur.r07@gmail.com>
2023-10-05 12:15:55 +0700

    fix: controller and routes auth

---
+++ app/controllers/auth.controller.js
@@ -0,0 +1,74 @@
+const db = require("../models");
+const config = require("../config/auth.config");
+const User = db.user;
+
+var jwt = require("jsonwebtoken");
+var bcrypt = require("bcryptjs");
+
+exports.register = async (req, res) => {
+  try {
+    const user = await User.create({
+      username: req.body.username,
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone,
+      password: bcrypt.hashSync(req.body.password, 8),
+    });
+
+    res.status(201).json({ message: 'Berhasil Registrasi !' });
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const user = await User.findOne({
+      where: {
+        username: req.body.username,
+      },
+    });
+
+    if (!user) {
+      return res.status(404).send({ message: "User tidak ditemukan." });
+    }
+
+    const passwordIsValid = bcrypt.compareSync(
+      req.body.password,
+      user.password
+    );
+
+    if (!passwordIsValid) {
+      return res.status(401).send({
+        message: "Password salah !",
+      });
+    }
+
+    const token = jwt.sign({ id: user.id }, config.secret, {
+      algorithm: "HS256",
+      allowInsecureKeySizes: true,
+      expiresIn: '10s',
+      // expiresIn: 86400, // 24 hours
+    });
+
+    return res.status(200).send({
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      accessToken: token,
+    });
+  } catch (error) {
+    return res.status(500).send({ message: error.message });
+  }
+};
+
+exports.signout = async (req, res) => {
+  try {
+    req.session = null;
+    return res.status(200).send({
+      message: "Anda berhasil keluar !",
+    });
+  } catch (err) {
+    this.next(err);
+  }
+};
