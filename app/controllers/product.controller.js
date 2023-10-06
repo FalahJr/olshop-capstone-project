41621223110fe626b0456c69a27d2b1d20296a8b
fatik07 <fatichur.r07@gmail.com>
2023-10-06 13:30:40 +0700

    fix: move jwt token to bearer token

--- app/controllers/product.controller.js
+++ app/controllers/product.controller.js
@@ -6,7 +6,7 @@ exports.getAllProducts = async (req, res) => {
     const products = await Product.findAll();
     res.status(200).json(products);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ success: false, message: error.message });
   }
 };
 
