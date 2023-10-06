bb723fc596bd278d90645209fe318b0f97a99e83
fatik07 <fatichur.r07@gmail.com>
2023-10-06 13:28:06 +0700

    fix: delete comment upload

--- app/config/upload.js
+++ app/config/upload.js
@@ -7,19 +7,19 @@ const storage = multer.diskStorage({
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    const ext = path.extname(file.originalname); // Ambil ekstensi file asli
-    cb(null, file.fieldname + '-' + uniqueSuffix + ext); // Nama file yang diunggah
+    const ext = path.extname(file.originalname);
+    cb(null, file.fieldname + '-' + uniqueSuffix + ext);
   },
 });
 
 const fileFilter = function (req, file, cb) {
-  const allowedFileTypes = /jpeg|jpg|png/; // Jenis file yang diizinkan (jpeg, jpg, png)
+  const allowedFileTypes = /jpeg|jpg|png/;
   const extname = allowedFileTypes.test(path.extname(file.originalname).toLowerCase());
   const mimetype = allowedFileTypes.test(file.mimetype);
   if (extname && mimetype) {
-    cb(null, true); // Jika tipe file sesuai, lanjutkan pengunggahan
+    cb(null, true);
   } else {
-    cb('Error: File harus berupa gambar (jpeg, jpg, png).', false); // Jika tipe file tidak sesuai, tolak pengunggahan
+    cb('Error: File harus berupa gambar (jpeg, jpg, png).', false);
   }
 };
 
